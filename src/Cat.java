2331128dc7b3d5cfb5971fb078ec1487dabb2ffe
Jonathan Zhang <jonathanz28@nycstudents.net>
2023-01-31 09:29:05 -0500

    ZhangU7T1

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CatRunner {
+import java.util.ArrayList;
+public class CatRunner {
+    public static void main(String[] args) {
+        ArrayList<Cat> cats = new ArrayList<Cat>();
+        Cat cat1 = new Cat("Barry");
+        Cat cat2 = new Cat("Henry");
+        Cat cat3 = new Cat("Winnie");
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        System.out.println(cats);
+
+        Cat cat4 = new Cat("Jonathan");
+        Cat replaced = cats.set(2, cat4);
+        System.out.println(cats);
+        System.out.println(replaced);
+
+        cat4.setName(cat4.getName() + " Meow");
+        System.out.println(cat4);
+        System.out.println(cats);
+
+    }
 }
